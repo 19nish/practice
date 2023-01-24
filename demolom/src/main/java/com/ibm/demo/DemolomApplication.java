8f8a89dd225d84a0e4d0b9f54ffb2d43ac233ace
19nish <71626550+19nish@users.noreply.github.com>
2023-01-24 12:56:17 +0530

    array list api

---
+++ demolom/src/main/java/com/ibm/demo/DemolomApplication.java
@@ -0,0 +1,21 @@
+package com.ibm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolomApplication.class, args);
+		/*
+		 * Student student= new Student(); student.setFirstName("Nishant");
+		 * student.setLastName("Chaudhari"); student.setId(1822207);
+		 * 
+		 * System.out.println(student.getFirstName());
+		 * System.out.println(student.getLastName());
+		 * System.out.println(student.getId());
+		 */
+	}
+
+}
---
+++ demolom/src/main/java/com/ibm/demo/Student.java
@@ -0,0 +1,37 @@
+package com.ibm.demo;
+
+
+
+
+
+
+
+public class Student {
+	private long id;
+	public Student(long id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	private String firstName;
+	private String lastName;
+}
